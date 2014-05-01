e4980e873d6582575679b10f869d84bd4af1f340
Vince Broz <vince@broz.cc>
2014-04-30 20:23:34 -0400

    Add system sorter

--- lib/cs_refresh/sorter.rb
+++ lib/cs_refresh/sorter.rb
@@ -1,7 +1,8 @@
 module CsRefresh
   # Sorts arrays of items
-  class Sorter
-    def sort(_arr)
+  class SystemSorter
+    def sort(arr)
+      arr.sort
     end
   end
 end
--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -2,13 +2,15 @@
 require 'spec_helper'
 require 'cs_refresh'
 
-describe CsRefresh::Sorter do
+
+
+describe CsRefresh::SystemSorter do
   cases = {
     [1, 2, 3, 4, 5, 7, 8, 6, 9, 10] => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   }
   cases.each do |unsorted, sorted|
     it "Should sort #{unsorted} into #{sorted}" do
-      expect(CsRefresh::Sorter.new.sort(unsorted)).to eq sorted
+      expect(CsRefresh::SystemSorter.new.sort(unsorted)).to eq sorted
     end
   end
 end
