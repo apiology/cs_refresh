3ea3feda55548800da8b5cc2f1f26e102514e12b
Vince Broz <vince@broz.cc>
2014-05-10 15:40:09 -0400

    Refactor Djisktra code

--- lib/cs_refresh/graph.rb
+++ lib/cs_refresh/graph.rb
@@ -43,59 +43,77 @@ module CsRefresh
 
     # Implement's Djikstra's algorithm
     def shortest_paths
-      distance = {}
-      prev = {}
-      distance_minimized = Set.new
-      prev[self] = nil
+      Dijkstra.new(self).shortest_paths
+    end
 
-      closest = PriorityQueue.new
-      closest.push(self, 0)
+    # Performs Dijkstra's algorithm on the specified graph starting
+    # node
+    class Dijkstra
+      def initialize(start)
+        @distance = {}
+        @prev = {}
+        @distance_minimized = Set.new
+        @prev[start] = nil
 
-      # Invariants:
-      #
-      # Each vertice is in priority queue zero or one times.
-      #
-      # Min in priority queue is always the final shortest path for
-      # that vertice.
-      #
-      until (node, node_distance = closest.delete_min).nil?
-        distance[node] = node_distance
-        distance_minimized.add(node)
+        @closest = PriorityQueue.new
+        @closest.push(start, 0)
+
+        dijkstraize!
+      end
+
+      def dijkstraize!
+        # Invariants:
+        #
+        # Each vertice is in priority queue zero or one times.
+        #
+        # Min in priority queue is always the final shortest path for
+        # that vertice.
+        #
+        until (node, node_distance = @closest.delete_min).nil?
+          explore_node(node, node_distance)
+        end
+      end
+
+      def shortest_paths
+        @shortest_paths ||= @distance.keys.map do |node|
+          [node, [@distance[node], format_path(path(node))]]
+        end.to_h
+      end
+
+      def explore_node(node, node_distance)
+        @distance[node] = node_distance
+        @distance_minimized.add(node)
 
         node.edges.each do |edge|
-          unless distance_minimized.include?(edge.target)
-            if relax(closest, edge.target, distance[node] + edge.weight)
-              prev[edge.target] = node
+          unless @distance_minimized.include?(edge.target)
+            if relax(edge.target, @distance[node] + edge.weight)
+              @prev[edge.target] = node
             end
           end
         end
       end
-      distance.keys.map do |node|
-        [node, [distance[node], format_path(path(prev, node))]]
-      end.to_h
-    end
 
-    def format_path(path)
-      path[1..path.length-2].reverse
-    end
-
-    def path(prev, node)
-      if node.nil?
-        []
-      else
-        [node].concat(path(prev, prev[node]))
+      def format_path(path)
+        path[1..path.length - 2].reverse
       end
-    end
 
-    def relax(closest,
-              node,
-              new_distance)
-      old_distance = closest[node]
-      if old_distance.nil? || old_distance > new_distance
-        closest[node] = new_distance
-        true
-      else
-        false
+      def path(node)
+        if node.nil?
+          []
+        else
+          [node].concat(path(@prev[node]))
+        end
+      end
+
+      def relax(node,
+                new_distance)
+        old_distance = @closest[node]
+        if old_distance.nil? || old_distance > new_distance
+          @closest[node] = new_distance
+          true
+        else
+          false
+        end
       end
     end
 
