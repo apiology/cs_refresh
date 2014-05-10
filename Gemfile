dfddf720cf085e9efac96902cfb293ea033f899c
Vince Broz <vince@broz.cc>
2014-05-10 14:09:21 -0400

    First cut at Dijkstra's algorithm

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 gem 'bundler'
 gem 'rake'
 gem 'quality'
+gem 'PriorityQueue' # used until I make my own fib heap in dijstra's algorithm
 gem 'algorithms' # reference implementations for tests, I swear!
 gem 'rspec', '>=3.0.0.beta2'
--- lib/cs_refresh/graph.rb
+++ lib/cs_refresh/graph.rb
@@ -1,3 +1,5 @@
+require 'priority_queue'
+
 module CsRefresh
   # Describes an edge between graph nodes
   class GraphEdge
@@ -37,6 +39,60 @@ module CsRefresh
 
     def inspect
       to_s
+    end
+
+    class HeapItem
+      attr_reader :node, :prev, :weight
+
+      def initialize(node, prev, weight)
+        @node = node
+        @prev = prev
+        @weight = weight
+      end
+
+      def <(other)
+        weight < other.weight
+      end
+    end
+
+    def shortest_paths
+      distance = {}
+      prev = {}
+      distance_minimized = Set.new
+      prev[self] = nil
+
+      closest = PriorityQueue.new
+      closest.push(self, 0)
+
+      # Invariants:
+      #
+      # Each vertice is in priority queue zero or one times.
+      #
+      # Min in priority queue is always the final shortest path for
+      # that vertice.
+      #
+      until (node, node_distance = closest.delete_min).nil?
+        distance[node] = node_distance
+        distance_minimized.add(node)
+
+        node.edges.each do |edge|
+          unless distance_minimized.include?(edge.target)
+            relax(closest,
+                  edge.target,
+                  distance[node] + edge.weight)
+          end
+        end
+      end
+      distance
+    end
+
+    def relax(closest,
+              node,
+              new_distance)
+      old_distance = closest[node]
+      if old_distance.nil? || old_distance > new_distance
+        closest[node] = new_distance
+      end
     end
 
     def calc_max_distances
